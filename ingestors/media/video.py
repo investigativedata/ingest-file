2b8f3282ada4fc3de0209d6a1b98b0533c5f6eba
Alex Ștefănescu <alex.stefanescu@pm.me>
2025-03-19 20:32:54 +0100

    Unload model after transcribing

--- ingestors/media/video.py
+++ ingestors/media/video.py
@@ -10,7 +10,7 @@ from ingestors.support.transcription import TranscriptionSupport
 log = logging.getLogger(__name__)
 
 
-class VideoIngestor(Ingestor, TimestampSupport):
+class VideoIngestor(Ingestor, TimestampSupport, TranscriptionSupport):
     MIME_TYPES = [
         "application/x-shockwave-flash",
         "video/quicktime",
--- ingestors/support/transcription.py
+++ ingestors/support/transcription.py
@@ -1,3 +1,4 @@
+import gc
 import logging
 
 from faster_whisper import WhisperModel
@@ -15,8 +16,35 @@ class TranscriptionSupport:
         https://github.com/SYSTRAN/faster-whisper/blob/master/faster_whisper/transcribe.py#L603
         beam_size: https://stackoverflow.com/questions/22273119/what-does-the-beam-size-represent-in-the-beam-search-algorithm 
         """
-        model = WhisperModel(MODEL_SIZE, device="cpu", compute_type="int8", cpu_threads=1, num_workers=1)
-        segments, _ = model.transcribe(file_path, beam_size=5)
 
-        for segment in segments:
-            entity.add("bodyText", f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
+        self.model = None
+
+        try:
+            # compute_type="float32"
+            self.model = WhisperModel(MODEL_SIZE, device="cpu", compute_type="int8", cpu_threads=1, num_workers=1)
+            log.info(f"Transcription model initialized successfully.")
+
+            segments, _ = self.model.transcribe(file_path, vad_filter=True, beam_size=5, no_speech_threshold=0.6, condition_on_previous_text=False)
+
+            for segment in segments:
+                entity.add("bodyText", f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
+                log.info(f"[{segment.start:.2f}s -> {segment.end:.2f}s] {segment.text}")
+        finally:
+            self._del_model()
+
+    def _del_model(self):
+        # https://github.com/SYSTRAN/faster-whisper/issues/660
+        self.model.model.unload_model()
+        
+        if hasattr(self.model, 'model'):
+            del self.model.model
+        if hasattr(self.model, 'feature_extractor'):
+            del self.model.feature_extractor
+        if hasattr(self.model, 'hf_tokenizer'):
+            del self.model.hf_tokenizer
+        
+        del self.model
+
+        gc.collect()
+
+        log.info("Transcription model removed from memory.")
